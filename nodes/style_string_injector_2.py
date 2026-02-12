a924ace9240587c91964537284f287628725df75
Martin Rizzo <martinrizzo@gmail.com>
2026-02-11 22:07:40 -0300

    feat: Introduce "StylePromptEncoder2" and "StyleStringInjector2" nodes
    This commit introduces the new nodes StylePromptEncoder2 and StyleStringInjector2,
    which are enhanced versions of their predecessors. These updated nodes retain the
    original functionality while adding a button to open the style gallery, making it
    easier for users to select styles.

--- nodes/style_string_injector_2.py
+++ nodes/style_string_injector_2.py
@@ -1,6 +1,6 @@
 """
-File    : style_string_injector.py
-Purpose : 
+File    : style_string_injector_2.py
+Purpose : Inject a visual style into a prompt. (version 2)
 Author  : Martin Rizzo | <martinrizzo@gmail.com>
 Date    : Jan 22, 2026
 Repo    : https://github.com/martin-rizzo/ComfyUI-ZImagePowerNodes
@@ -19,8 +19,8 @@ from .lib.style_group            import StyleGroup
 from ..styles.predefined_styles  import PREDEFINED_STYLE_GROUPS
 
 
-class StyleStringInjector(io.ComfyNode):
-    xTITLE         = "Style String Injector (old version)"
+class StyleStringInjector2(io.ComfyNode):
+    xTITLE         = "Style String Injector"
     xCATEGORY      = ""
     xCOMFY_NODE_ID = ""
     xDEPRECATED    = False
@@ -38,12 +38,12 @@ class StyleStringInjector(io.ComfyNode):
                 "according to the selected style"
             ),
             inputs=[
-                io.Combo.Input ("category", options=cls.category_names(), default=cls.default_category_name(),
-                                tooltip="The category of styles you want to select from.",
-                               ),
                 io.Combo.Input ("style", options=cls.style_names(), default=cls.default_style_name(),
-                                tooltip="The style you want for your image.",
+                                tooltip="The visual style you want for your image.",
                                ),
+                io.Custom("ZIPN_STYLE_GALLERY").Input("gallery",
+                                                      tooltip="Open the style gallery to see all available styles."
+                                                     ),
                 io.String.Input("string", multiline=True, dynamic_prompts=True, force_input=True,
                                 tooltip="The prompt to modify.",
                                ),
@@ -56,7 +56,6 @@ class StyleStringInjector(io.ComfyNode):
     #__ FUNCTION __________________________________________
     @classmethod
     def execute(cls,
-                category      : str,
                 style         : str,
                 string        : str,
                 ) -> io.NodeOutput:
@@ -76,19 +75,11 @@ class StyleStringInjector(io.ComfyNode):
     #__ VALIDATION ________________________________________
     @classmethod
     def validate_inputs(cls, **kwargs) -> bool | str:
-        if kwargs["category"] not in cls.category_names():
-            return f"The category name '{kwargs['style_type']}' is invalid. May be the node is from an older version."
         return True
 
 
 
     #__ internal functions ________________________________
-
-    @classmethod
-    def category_names(cls) -> list[str]:
-        """Returns all available category names."""
-        return [ group.category for group in PREDEFINED_STYLE_GROUPS ]
-
 
     @classmethod
     def style_names(cls) -> list[str]:
@@ -97,11 +88,6 @@ class StyleStringInjector(io.ComfyNode):
         for style_group in PREDEFINED_STYLE_GROUPS:
             names.extend( style_group.get_names(quoted=True) )
         return names
-
-
-    @classmethod
-    def default_category_name(cls) -> str:
-        return PREDEFINED_STYLE_GROUPS[0].category
 
 
     @classmethod
